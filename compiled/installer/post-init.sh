9504e90328082faafa5e819e7839cdece84c0c1d
Negamann <negamann303@hotmail.com>
2016-07-30 21:42:05 -0600

    POST-INIT: Added Intelliactive Custom Settings

--- compiled/installer/post-init.sh
+++ compiled/installer/post-init.sh
@@ -16,19 +16,35 @@ CPU_MIN="300000" # [300000 384000 600000 787200 998400 1094400 1190400]
 CPU_ONLINE="1" #(1=All Cores Enabled)
 
 # CPU Governor
-CPU_GOV="ondemand" # [darkness intellidemand intelliactive interactiveX thunderx pegasusq smartmax ondemand userspace powersave performance]
+CPU_GOV="intelliactive" # [darkness intellidemand intelliactive interactiveX thunderx pegasusq smartmax ondemand userspace powersave performance]
 
 # CPU Thermal Control
 THERMAL_VDD="0"
 THERMAL_CORE="1"
 
 # Ondemand Settings(if chosen)
-OND_SAMP_RATE="20000"
+OND_SAMP_RATE="30000"
 OND_SAMP_RATE_MIN="10000"
-OND_SAMP_RATE_MAX="30000"
-OND_SAMP_DOWN_FACT="3"
-OND_UP_THRESH="60"
+OND_SAMP_RATE_MAX="50000"
+OND_SAMP_DOWN_FACT="2"
+OND_UP_THRESH="75"
 OND_IO_BUSY="1"
+
+# Intelliactive Settings(if chosen)
+INTELLI_ABOVE_HISPEED_DELAY="10000"
+INTELLI_BOOST="1"
+INTELLI_BOOSTPULSE_DUR="10000"
+INTELLI_HISPEED_LOAD="35"
+INTELLI_HISPEED_FREQ="300000"
+INTELLI_MIN_SAMP_TIME="20000"
+INTELLI_SAMP_DWN_FACT="1"
+INTELLI_SYNC_FREQ="787200"
+INTELLI_TARGET_LOAD="80"
+INTELLI_TIMR_RATE="20000"
+INTELLI_TIMR_SLACK="80000"
+INTELLI_2_PHASE_FREQ="600000,787200,1094400,1190400"
+INTELLI_UP_THRESH_ANY_FREQ="787200"
+INTELLI_UP_THRESH_ANY_LOAD="85"
 
 # GPU Frequency
 GPU_FREQ_MAX="533000000" # [200000000 320000000 450000000 533000000]
@@ -119,19 +135,6 @@ else
 	echo 0 > /sys/devices/system/cpu/cpu3/online
 fi
 
-# Thermal Control
-echo $THERMAL_CORE > /sys/module/msm_thermal/core_control/enabled
-echo $THERMAL_VDD > /sys/module/msm_thermal/vdd_restriction/enabled
-
-# CPU BOOST
-echo 1 > /sys/module/cpu_boost/parameters/cpu_boost
-echo 100 > /sys/module/cpu_boost/parameters/boost_ms
-echo $CPU_BOOST_MAX > /sys/module/cpu_boost/parameters/input_boost_freq
-echo 100 > /sys/module/cpu_boost/parameters/input_boost_ms
-echo 1 > /sys/module/cpu_boost/parameters/load_based_syncs
-echo 15 > /sys/module/cpu_boost/parameters/migration_load_threshold
-echo $CPU_MAX > /sys/module/cpu_boost/parameters/sync_threshold
-
 echo $CPU_MIN > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo $CPU_MAX > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
@@ -157,5 +160,44 @@ if [ $GOVERNOR = "intellidemand" ]; then
 	echo "1" > /sys/devices/system/cpu/cpufreq/intellidemand/ignore_nice_load
 	echo $OND_SAMP_DOWN_FACT > /sys/devices/system/cpu/cpufreq/intellidemand/sampling_down_factor
 fi
+
+if [ $GOVERNOR = "intelliactive" ]; then
+	echo $INTELLI_ABOVE_HISPEED_DELAY > /sys/devices/system/cpu/cpufreq/intelliactive/above_hispeed_delay
+	echo $INTELLI_BOOST > /sys/devices/system/cpu/cpufreq/intelliactive/boost
+	echo $INTELLI_BOOSTPULSE_DUR > /sys/devices/system/cpu/cpufreq/intelliactive/boostpulse_duration
+	echo $INTELLI_HISPEED_LOAD > /sys/devices/system/cpu/cpufreq/intelliactive/go_hispeed_load
+	echo $INTELLI_HISPEED_FREQ > /sys/devices/system/cpu/cpufreq/intelliactive/hispeed_freq
+	echo $OND_IO_BUSY > /sys/devices/system/cpu/cpufreq/intelliactive/io_is_busy
+	echo $INTELLI_MIN_SAMP_TIME > /sys/devices/system/cpu/cpufreq/intelliactive/min_sample_time
+	echo $INTELLI_SAMP_DWN_FACT > /sys/devices/system/cpu/cpufreq/intelliactive/sampling_down_factor
+	echo $INTELLI_SYNC_FREQ > /sys/devices/system/cpu/cpufreq/intelliactive/sync_freq
+	echo $INTELLI_TARGET_LOAD > /sys/devices/system/cpu/cpufreq/intelliactive/target_loads
+	echo $INTELLI_TIMR_RATE > /sys/devices/system/cpu/cpufreq/intelliactive/timer_rate
+	echo $INTELLI_TIMR_SLACK > /sys/devices/system/cpu/cpufreq/intelliactive/timer_slack
+	echo $INTELLI_2_PHASE_FREQ > /sys/devices/system/cpu/cpufreq/intelliactive/two_phase_freq
+	echo $INTELLI_UP_THRESH_ANY_FREQ > /sys/devices/system/cpu/cpufreq/intelliactive/up_threshold_any_cpu_freq
+	echo $INTELLI_UP_THRESH_ANY_LOAD > /sys/devices/system/cpu/cpufreq/intelliactive/up_threshold_any_cpu_load
+fi
+
+# Thermal Control
+echo $THERMAL_CORE > /sys/module/msm_thermal/core_control/enabled
+echo $THERMAL_VDD > /sys/module/msm_thermal/vdd_restriction/enabled
+echo $CPU_MIN > /sys/module/msm_thermal/vdd_restriction/vdd-apps/value
+echo 7 > /sys/module/msm_thermal/vdd_restriction/vdd-dig/value
+
+INTELLI_BOOST=`cat /sys/devices/system/cpu/cpufreq/intelliactive/boost`
+
+# CPU BOOST
+if [ $INTELLI_BOOST = "1" ]; then
+	echo 0 > /sys/module/cpu_boost/parameters/cpu_boost
+else
+	echo 1 > /sys/module/cpu_boost/parameters/cpu_boost
+fi
+echo 100 > /sys/module/cpu_boost/parameters/boost_ms
+echo $CPU_BOOST_MAX > /sys/module/cpu_boost/parameters/input_boost_freq
+echo 100 > /sys/module/cpu_boost/parameters/input_boost_ms
+echo 1 > /sys/module/cpu_boost/parameters/load_based_syncs
+echo 15 > /sys/module/cpu_boost/parameters/migration_load_threshold
+echo $CPU_MAX > /sys/module/cpu_boost/parameters/sync_threshold
 
 echo "Post-init finished ..."
