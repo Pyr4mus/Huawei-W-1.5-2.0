0bdde791f89356d6b13e33f50cdbde681f31ce12
negamann303 <negamann303@gmail.com>
2016-09-06 23:38:30 -0600

    Superuser: Remove Old Superuser

--- compiled/installer/setup/su.sh
+++ compiled/installer/setup/su.sh
@@ -4,6 +4,10 @@ if [ ! -e /data/su.img ]; then
 	mv /tmp/su.img /data/su.img
 fi
 
+if [ -e /system/app/superuser/Superuser.apk ]; then
+	rm -rf /system/app/superuser
+fi
+
 if [ ! -e /system/app/Superuser/Superuser.apk ]; then
 	mkdir /system/app/Superuser
 	mv /tmp/Superuser.apk /system/app/Superuser/Superuser.apk
